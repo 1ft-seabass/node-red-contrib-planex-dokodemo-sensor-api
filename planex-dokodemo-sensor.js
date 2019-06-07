2c55cdd20402a39d00bb37fedd265b0328b86b05
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-06-07 23:53:23 +0900

    added latest node

--- planex-dokodemo-sensor.js
+++ planex-dokodemo-sensor.js
@@ -1,4 +1,6 @@
 var request = require('request');
+var moment = require('moment');
+
 
 module.exports = function(RED) {
     function PlanexDokodemoSensorAPI(config) {
@@ -13,8 +15,8 @@ module.exports = function(RED) {
             var _mac = this.credentials.sensor_mac_address;
             var _type = config.sensor_type;
 
-            var _from = msg.payload.from || config.sensor_datetime_from;
-            var _to = msg.payload.to || config.sensor_datetime_to;
+            var _from = msg.from || config.sensor_datetime_from;
+            var _to = msg.to || config.sensor_datetime_to;
 
             var _data = {
                 type:'"' + _type + '"',
@@ -41,7 +43,63 @@ module.exports = function(RED) {
             
         });
     }
+
+    function PlanexDokodemoSensorLatestDataAPI(config) {
+        
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            
+            var _currentDate = new Date();
+
+            var _token = this.credentials.sensor_token;
+            var _mac = this.credentials.sensor_mac_address;
+            var _type = config.sensor_type;
+            var _from = moment().utcOffset(0).add(-1,"minutes").format("YYYY-MM-DD HH:mm:ss");
+            var _to = moment().utcOffset(0).format("YYYY-MM-DD HH:mm:ss");
+
+            console.log(_from);
+            console.log(_to);
+
+            var _data = {
+                type:'"' + _type + '"',
+                mac:'"' + _mac + '"',
+                from:'"' + _from + '"',
+                to:'"' + _to + '"',
+                token:'"' + _token + '"'
+            };
+
+            var headers = {}
+
+            var options = {
+                uri: 'https://svcipp.planex.co.jp/api/get_data.php',
+                headers: headers,
+                qs: _data
+            }
+
+            request.get(options, function (error, response, body) {
+                var datas = JSON.parse(body);
+                if(datas.length > 0){
+                    msg.payload = datas.pop();
+                } else {
+                    msg.payload = [];
+                }
+                msg.response = response;
+                node.send(msg);
+            })
+
+            
+        });
+    }
+
     RED.nodes.registerType("planex-dokodemo-sensor",PlanexDokodemoSensorAPI,{
+        credentials: {
+            sensor_token: {type:"text"},
+            sensor_mac_address: {type:"text"}
+        }
+    });
+
+    RED.nodes.registerType("planex-dokodemo-sensor-latest",PlanexDokodemoSensorLatestDataAPI,{
         credentials: {
             sensor_token: {type:"text"},
             sensor_mac_address: {type:"text"}
